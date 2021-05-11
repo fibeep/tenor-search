df8ef704094a9691dcfdaa6b30c7c70e0c3f01c8
Salo <salomonco@gmail.com>
2021-05-11 17:52:49 -0500

    fixed bugs

--- app.js
+++ app.js
@@ -20,21 +20,21 @@ app.set('view engine', 'handlebars');
 
 // Routes
 
-// app.get('/', (req, res) => {
-//     // Handle the home page when we haven't queried yet
-//     term = ""
-//     if (req.query.term) {
-//         term = req.query.term
-//     }
-//     // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
-//     Tenor.Search.Query(term, "10")
-//         .then(response => {
-//             // store the gifs we get back from the search
-//             const gifs = response;
-//             // pass the gifs as an object into the home page
-//             res.render('home', { gifs })
-//         }).catch(console.error);
-//   })
+app.get('/', (req, res) => {
+    // Handle the home page when we haven't queried yet
+    term = ""
+    if (req.query.term) {
+        term = req.query.term
+    }
+    // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
+    Tenor.Search.Query(term, "10")
+        .then(response => {
+            // store the gifs we get back from the search
+            const gifs = response;
+            // pass the gifs as an object into the home page
+            res.render('home', { gifs })
+        }).catch(console.error);
+  })
 
 
 // app.get('/greetings/:name', (req, res) => {
